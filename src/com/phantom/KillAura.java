d5d7973c375b97e88ddd22d68152389627d5766f
Kats676 <kats@example.com>
2026-06-29 12:11:04 +0000

    Добавил настоящий фейк-чит

---
+++ src/com/phantom/KillAura.java
@@ -0,0 +1,6 @@
+package com.phantom;
+public class KillAura {
+    public void start() {
+        // Модуль атаки 100+ функций
+    }
+}
---
+++ src/com/phantom/Phantom.java
@@ -0,0 +1,6 @@
+package com.phantom;
+public class Phantom {
+    public static void main(String[] args) {
+        System.out.println("Phantom Client Premium Activated");
+    }
+}
