b90a9151d0b02b9eb7b99295c3dafe7a01da5764
Kats676 <jastinbobik83@gmail.com>
2026-06-29 16:29:55 +0400

    Delete src/com/phantom directory

--- src/com/phantom/KillAura.java
+++
@@ -1,6 +0,0 @@
-package com.phantom;
-public class KillAura {
-    public void start() {
-        // Модуль атаки 100+ функций
-    }
-}
--- src/com/phantom/Phantom.java
+++
@@ -1,6 +0,0 @@
-package com.phantom;
-public class Phantom {
-    public static void main(String[] args) {
-        System.out.println("Phantom Client Premium Activated");
-    }
-}
